70324e5af1b0f34c1eb8cb6dd084ab648d0fa924
Sakshi Pokhria <sakshipokhria1997@gmail.com>
2024-07-25 16:51:19 +0530

    created service file for business logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,77 +1,15 @@
-import UserModel from "../models/users.js";
-import bcrypt from "bcrypt";
+import UserService from "../services/userService.js";
 
 class UserController {
   static userRegistration = async (req, res) => {
-    const { name, email, password, confirm_password, tc } = req.body;
-    if (name && email && password && confirm_password && tc) {
-      const userExist = await UserModel.findOne({ email });
-      if (userExist) {
-        res
-          .status(400)
-          .send({
-            status: "failed",
-            message: "User Already exist with this email",
-          });
-      }
-      if (password !== confirm_password) {
-        res
-          .status(400)
-          .send({
-            status: "failed",
-            message: "Password & confirm password do not match",
-          });
-      }
-      try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const user = new UserModel({
-          name,
-          email,
-          password: hashedPassword,
-          tc,
-        });
-        await user.save();
-        res
-          .status(201)
-          .send({ status: "success", message: "User registered successfully" });
-      } catch (error) {
-        res
-          .status(500)
-          .send({ status: "failed", message: "Failed to register the user" });
-      }
-    } else {
-      res
-        .status(400)
-        .send({ status: "failed", message: "All fields are required" });
-    }
+    const result = await UserService.registerUser(req.body);
+    res.status(result.status).send({ status: result.status === 201 ? "success" : "failed", message: result.message });
   };
 
-  static userLogin = async(req,res) => {
-    const {email,password} = req.body;
-    if(email && password){
-        const user = await UserModel.findOne({email});
-        if(!user){
-            res.status(400).send({status:"failed",message:"User with the email do not exist"});
-        }
-        try{
-            const passwordMatched = await bcrypt.compare(password,user.password);
-            if(user && passwordMatched){
-                res.status(200).send({status:"success"});
-            }else{
-                res.status(400).send({status:"failed",message:"password do not match"});
-            }
-        }catch(error){
-            res
-            .status(500)
-            .send({ status: "failed", message: "Failed to Login" });
-        }
-    }else{
-        res
-        .status(400)
-        .send({ status: "failed", message: "All fields are required" });
-    }
-  }
+  static userLogin = async (req, res) => {
+    const result = await UserService.loginUser(req.body);
+    res.status(result.status).send({ status: result.status === 200 ? "success" : "failed", message: result.message });
+  };
 }
 
 export default UserController;
---
+++ services/userService.js
@@ -0,0 +1,57 @@
+import UserModel from "../models/users.js";
+import bcrypt from "bcrypt";
+
+class UserService {
+  static async registerUser(data) {
+    const { name, email, password, confirm_password, tc } = data;
+    if (name && email && password && confirm_password && tc) {
+      const userExist = await UserModel.findOne({ email });
+      if (userExist) {
+        return { status: 400, message: "User already exists with this email" };
+      }
+      if (password !== confirm_password) {
+        return { status: 400, message: "Password & confirm password do not match" };
+      }
+      try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = new UserModel({
+          name,
+          email,
+          password: hashedPassword,
+          tc,
+        });
+        await user.save();
+        return { status: 201, message: "User registered successfully" };
+      } catch (error) {
+        return { status: 500, message: "Failed to register the user" };
+      }
+    } else {
+      return { status: 400, message: "All fields are required" };
+    }
+  }
+
+  static async loginUser(data) {
+    const { email, password } = data;
+    if (email && password) {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return { status: 400, message: "User with the email does not exist" };
+      }
+      try {
+        const passwordMatched = await bcrypt.compare(password, user.password);
+        if (passwordMatched) {
+          return { status: 200, message: "Login successful" };
+        } else {
+          return { status: 400, message: "Password does not match" };
+        }
+      } catch (error) {
+        return { status: 500, message: "Failed to login" };
+      }
+    } else {
+      return { status: 400, message: "All fields are required" };
+    }
+  }
+}
+
+export default UserService;
